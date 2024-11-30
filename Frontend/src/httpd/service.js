2d7567b058712b934da1503245648242bac51c31
Ak4shKr <akashkmr7667@gmail.com>
2024-11-30 15:37:09 +0530

    url added

--- Frontend/src/httpd/service.js
+++ Frontend/src/httpd/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5000/api/admin",
+  baseURL: "https://ymca-exam.onrender.com/api/admin",
   withCredentials: true,
 });
 
