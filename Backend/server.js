958dd0e05122892e0e178df4f7d622f2d30a40b6
Ak4shKr <akashkmr7667@gmail.com>
2024-11-30 15:43:28 +0530

    url updated for cors

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
